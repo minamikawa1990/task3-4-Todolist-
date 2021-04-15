421aa34d730693e2cbcb2b2402beba1f46054863
minamikawa1990 <ut.magnon@gmail.com>
2021-04-15 13:13:43 +0900

    displayTodosに引数

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 'use strict'
 
 {
-    
-    
-    const radioCheck = document.getElementsByClassName('radioCheck');
-
-    if(radioCheck[0].cheked) {
-
-    }
-    
     const add = document.getElementById('add');
     const input = document.getElementById('input');
     const tasks = document.getElementById('tasks');
@@ -28,9 +20,7 @@
             }else if(createBtnStatus.textContent === '完了') {
                 createBtnStatus.textContent = '作業中';
             }
-            
-            todos[index].status = createBtnStatus.textContent;
-            
+            todos[index].status = createBtnStatus.textContent; 
         });
         return createBtnStatus;
     };
@@ -47,7 +37,7 @@
         return createBtnRemove;
     };
 
-    function displayTodos() {
+    function displayTodos(todos) {
         tasks.innerText = '';
         todos.forEach(todo => {
             const todoId = tasks.rows.length;//tbody内の行の個数をId番号に指定
@@ -80,62 +70,30 @@
         console.log(todos);
     })
 
-
     const getRadioAll = document.getElementById('radio-all');
     const getRadioWorking = document.getElementById('radio-working');
     const getRadioComplete = document.getElementById('radio-complete');
 
     getRadioAll.addEventListener('click', () => {
         console.log('aaa');
-        addTask();
+        displayTodos();
     })
     getRadioWorking.addEventListener('click', () => {
-        const workingArray= todos.filter( function(value) {
+        const workingArray　= todos.filter( function(value) {
             return value.status === '作業中';
         });
-        tasks.style.display ='none';
-        tasks.innerText = '';
-        workingArray.forEach(todo => {
-        const todoId = tasks.rows.length;//tbody内の行の個数をId番号に指定
-        const row = tasks.insertRow(-1);//変数rowをtasksの最終行に追加
-        row.classList.add('tasks');//rowにtasksクラスをつける
-        const id = row.insertCell(0);//変数idをrowの1番目に挿入
-        const comment = row.insertCell(1);//変数commentをrowの2番目に挿入
-        const status = row.insertCell(2);//変数statusをrowの3番目に挿入
-        const remove = row.insertCell(3);//変数removeをrowの4番目に挿入
-        id.innerText = todoId;//idのテキストをtodoIdに指定
-        comment.innerText = workingArray.task;//コメントのテキストをオブジェクトの値で指定
-        
-        removeTask(remove, row);
-        addStatus(status, row);
-        });
-        
-        console.log(result);
+        displayTodos(workingArray);
+        console.log(workingArray);
     });
+
     getRadioComplete.addEventListener('click', () => {
         const completeArray = todos.filter( function(value) {
             return value.status === '完了';
         });
-        tasks.style.display ='none';
-        tasks.innerText = '';
-        completeArray.forEach(todo => {
-        const todoId = tasks.rows.length;//tbody内の行の個数をId番号に指定
-        const row = tasks.insertRow(-1);//変数rowをtasksの最終行に追加
-        row.classList.add('tasks');//rowにtasksクラスをつける
-        const id = row.insertCell(0);//変数idをrowの1番目に挿入
-        const comment = row.insertCell(1);//変数commentをrowの2番目に挿入
-        const status = row.insertCell(2);//変数statusをrowの3番目に挿入
-        const remove = row.insertCell(3);//変数removeをrowの4番目に挿入
-        id.innerText = todoId;//idのテキストをtodoIdに指定
-        comment.innerText = completeArray.task;//コメントのテキストをオブジェクトの値で指定
         
-        removeTask(remove, row);
-        addStatus(status, row);
-        });
-        console.log(result);
-        
-    })
-
+        displayTodos(completeArray);
+        console.log(completeArray);
+    });
 
 }
 
